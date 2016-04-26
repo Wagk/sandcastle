e20f19c96295a799ea968309d297169451c5040a
Pang Tun Jiang <polkafreaku@gmail.com>
2016-04-27 02:31:27 +0800

    Added `sandcastle_graphics` project, for graphics.
    Renamed `SANDCASTLEAPI` to `SANDCASTLE_API`, because it looks clearer
    
    output files and intermediate files now go into their own
    named folders within `output` and `artifacts`
    
    Set up a build dependency on Application and Sandcastle.
    
    `sandcastle_graphics` is set up to be a DLL with the same
    `SANDCASTLE_API` as the `sandcastle` project

---
+++ sandcastle_graphics/drawrequest.h
@@ -0,0 +1,36 @@
+/*!************************************************************
+	Created	:	2016/04/27
+	Intent	:		
+		
+    Defines a request you submit to the renderer for further
+    rendering
+
+	Usage	:
+		```
+			
+		```
+	Note	:
+		
+**************************************************************/
+#ifndef drawrequest_h__
+#define drawrequest_h__
+
+namespace Graphics
+{
+
+  struct Drawrequest
+  {
+
+  };
+
+} //namespace Graphics
+
+#endif // drawrequest_h__
+/*!************************************************************
+	Todo:
+			
+	References:
+		
+**************************************************************/
+
+
