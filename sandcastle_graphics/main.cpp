11d6ee8974e54a7d3842aba45dc7ab9fbfd2d06b
Pang Tun Jiang <polkafreaku@gmail.com>
2016-04-27 23:02:22 +0800

    turned sandcastle_graphics back into an executable
    We'll turn it back into a dll once we settled all the library
    stuff

---
+++ sandcastle_graphics/main.cpp
@@ -0,0 +1,22 @@
+/*!************************************************************
+	Created	:	2016/04/27
+	Note	:
+		
+    Until we're comfortable with attaching all these new
+    fancy libraries we're just going to build this as a separate
+    application
+
+**************************************************************/
+
+int main()
+{
+
+}
+
+
+/*!************************************************************
+	Todo:
+			
+	References:
+		
+**************************************************************/
