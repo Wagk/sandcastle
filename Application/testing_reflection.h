214db60b974d15d756654a30c9a55e8efc22b748
pang.t <polkafreaku@gmail.com>
2016-06-03 17:08:58 +0800

    Gonna start testing the reflection

---
+++ Application/testing_reflection.h
@@ -0,0 +1,15 @@
+/*!************************************************************
+Created:	2016/06/03
+Intent:
+
+**************************************************************/
+#pragma once
+
+#include "sandcastle/reflection_reflectable.h"
+
+namespace Testing
+{
+
+
+
+} //namespace Testing
---
+++ sandcastle_graphics/testing_graphics.h
@@ -0,0 +1,17 @@
+/*!************************************************************
+Created:	2016/06/03
+Intent:
+
+**************************************************************/
+#pragma once
+
+#include "export_macros.h"
+
+namespace Testing
+{
+  namespace Graphics
+  {
+    SANDCASTLE_API int GraphicsTest(void);
+  }
+}
+
