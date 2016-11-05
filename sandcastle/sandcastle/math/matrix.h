218a56015247cdd015b39d6dc7864a06f1185b63
Pang Tun Jiang <pang.tun.jiang@gmail.com>
2016-11-06 00:44:13 +0800

    updating project
    not sure how we're going forward with this
    
    We might decide on doing another template implementation, or maybe we can
    hardcode this one a bit more.

---
+++ sandcastle/sandcastle/math/matrix.h
@@ -0,0 +1,9 @@
+#ifndef math_matrix_h__
+#define math_matrix_h__
+
+namespace sandcastle::math
+{
+
+}
+
+#endif
---
+++ sandcastle/sandcastle/math/vector.h
@@ -0,0 +1,10 @@
+#ifndef math_vector_h__
+#define math_vector_h__
+
+namespace sandcastle::math
+{
+
+
+}
+
+#endif
