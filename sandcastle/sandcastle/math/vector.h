fc82781d829616f1aed388bd493f52eb09fcd84a
Pang Tun Jiang <pang.tun.jiang@gmail.com>
2016-11-07 23:26:15 +0800

    shifted glm and future libraries to the `lib` folder

--- sandcastle/sandcastle/math/vector.h
+++ sandcastle/sandcastle/math/vector.h
@@ -4,6 +4,22 @@
 namespace sandcastle::math
 {
 
+	class vector3
+	{
+	public:
+
+		//TODO:
+		//swizzling
+		//
+
+	private:
+
+		float arr[3];
+
+	};
+
+	using vec3 = vector3;
+	//using vec2 = vector2;
 
 }
 
