1dfe4be267d82d95c817d314b4b85997f0672970
pang.t <polkafreaku@gmail.com>
2016-11-09 21:27:56 +0800

    adding glfw, can't install the vulkan sdk here

--- sandcastle/sandcastle/main.cpp
+++ sandcastle/sandcastle/main.cpp
@@ -14,6 +14,14 @@
 #include <sstream>
 #include <memory>
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+//#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 /*
 	Each thread has a worker object
 */
@@ -107,8 +115,37 @@ void math_test()
 	};
 }
 
+int glfw_test()
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+	uint32_t extensionCount = 0;
+	//vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+	std::cout << extensionCount << " extensions supported" << std::endl;
+
+	glm::mat4 matrix;
+	glm::vec4 vec;
+	auto test = matrix * vec;
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+
+	return 0;
+}
+
 int main(int argc, char* argv[])
 {
 	//concurrency_test();
-	math_test();
+	//math_test();
+
+
 }
