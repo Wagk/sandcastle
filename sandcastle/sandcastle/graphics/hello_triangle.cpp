35eda938ce81c33786db10c3072f77082711b12c
Pang Tun Jiang <pang.tun.jiang@gmail.com>
2016-11-23 02:25:17 +0800

    shifted functions to cpp to encapsulate constants

--- sandcastle/sandcastle/graphics/hello_triangle.cpp
+++ sandcastle/sandcastle/graphics/hello_triangle.cpp
@@ -2,5 +2,93 @@
 
 namespace sandcastle::graphics
 {
+	const int width = 800;
+	const int height = 600;
 
+	inline void simpletriangle::run()
+	{
+
+		init();
+		initvulkan();
+		main_loop();
+
+	}
+
+	inline void simpletriangle::init()
+	{
+
+		glfwInit();
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+		_window = glfwCreateWindow(width, height, "Vulkan", nullptr, nullptr);
+
+	}
+
+	inline void simpletriangle::initvulkan()
+	{
+		createinstance();
+	}
+
+	inline void simpletriangle::main_loop()
+	{
+
+		while (!glfwWindowShouldClose(_window))
+		{
+			glfwPollEvents();
+		}
+
+	}
+
+	//assumes the instance is already initialized
+	inline std::vector<VkExtensionProperties> simpletriangle::enumerateExtensions() const
+	{
+
+		uint32_t extension_count = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, nullptr);
+
+		std::vector<VkExtensionProperties> extensions(extension_count);
+
+		vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, extensions.data());
+
+		return extensions;
+
+	}
+
+	inline void simpletriangle::createinstance()
+	{
+
+		VkApplicationInfo appinfo = {};
+		appinfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appinfo.pApplicationName = "Hello Triangle";
+		appinfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appinfo.pEngineName = "No Engine";
+		appinfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appinfo.apiVersion = VK_API_VERSION_1_0;
+
+		unsigned int glfw_extension_count = 0;
+		const char** glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
+
+		VkInstanceCreateInfo createinfo = {};
+		createinfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createinfo.pApplicationInfo = &appinfo;
+		createinfo.enabledExtensionCount = glfw_extension_count;
+		createinfo.ppEnabledExtensionNames = glfw_extensions;
+		createinfo.enabledLayerCount = 0;
+
+		if (vkCreateInstance(&createinfo, nullptr, _instance.replace()) != VK_SUCCESS)
+		{
+			throw std::runtime_error("Failed to create instance!");
+		}
+
+		std::vector<VkExtensionProperties> extensions = enumerateExtensions();
+
+		std::cout << "Available Extensions:" << std::endl;
+		for (const auto& exts : extensions)
+		{
+			std::cout << "\t" << exts.extensionName << std::endl;
+		}
+
+	}
 }
--- sandcastle/sandcastle/graphics/hello_triangle.h
+++ sandcastle/sandcastle/graphics/hello_triangle.h
@@ -15,100 +15,19 @@ namespace sandcastle::graphics
 	{
 	public:
 
-		static const int width = 800;
-		static const int height = 600;
-
-		void run()
-		{
-
-			init();
-			initvulkan();
-			main_loop();
-
-		}
+		void run();
 
 	private:
 
 		GLFWwindow* _window;
 		vkhandle<VkInstance> _instance{vkDestroyInstance};
 
-		void init()
-		{
+		void init();
+		void initvulkan();
+		void main_loop();
+		std::vector<VkExtensionProperties> enumerateExtensions() const;
 
-			glfwInit();
-
-			glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-			glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-			
-			_window = glfwCreateWindow(width, height, "Vulkan", nullptr, nullptr);
-
-		}
-
-		void initvulkan()
-		{
-			createinstance();
-		}
-
-		void main_loop()
-		{
-
-			while (!glfwWindowShouldClose(_window)) 
-			{
-				glfwPollEvents();
-			}
-
-		}
-
-		//assumes the instance is already initialized
-		std::vector<VkExtensionProperties> enumerateExtensions() const
-		{
-
-			uint32_t extension_count = 0;
-			vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, nullptr);
-
-			std::vector<VkExtensionProperties> extensions(extension_count);
-
-			vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, extensions.data());
-
-			return extensions;
-
-		}
-
-		void createinstance()
-		{
-
-			VkApplicationInfo appinfo = {};
-			appinfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-			appinfo.pApplicationName = "Hello Triangle";
-			appinfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-			appinfo.pEngineName = "No Engine";
-			appinfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-			appinfo.apiVersion = VK_API_VERSION_1_0;
-
-			unsigned int glfw_extension_count = 0;
-			const char** glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
-
-			VkInstanceCreateInfo createinfo = {};
-			createinfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-			createinfo.pApplicationInfo = &appinfo;
-			createinfo.enabledExtensionCount = glfw_extension_count;
-			createinfo.ppEnabledExtensionNames = glfw_extensions;
-			createinfo.enabledLayerCount = 0;
-
-			if (vkCreateInstance(&createinfo, nullptr, _instance.replace()) != VK_SUCCESS)
-			{
-				throw std::runtime_error("Failed to create instance!");
-			}
-
-			std::vector<VkExtensionProperties> extensions = enumerateExtensions();
-
-			std::cout << "Available Extensions:" << std::endl;
-			for (const auto& exts : extensions)
-			{
-				std::cout << "\t" << exts.extensionName << std::endl;
-			}
-
-		}
+		void createinstance();
 
 	};
 }
